cd838c649b1bf1e6c4815201dc3dee080636bf16
Connor Meehan <18220073+cmeehan1991@users.noreply.github.com>
2024-04-18 11:45:27 -0400

    Updating base route

--- src/app/lib/constants/routes.ts
+++ src/app/lib/constants/routes.ts
@@ -1,4 +1,4 @@
-//export const BASE = `http://localhost:8080`;
-export const BASE = `https://api.utterfare.com`;
+export const BASE = `http://localhost:8080`;
+//export const BASE = `https://api.utterfare.com`;
 export const VENDOR_PROFILE_ROUTE = `${BASE}/vendor/profile`;
 export const SEARCH_ROUTE = `${BASE}/search`;
