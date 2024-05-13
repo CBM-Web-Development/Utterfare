97f79d05c8e682ee84fd70739f494faa82cb2283
Connor Meehan <18220073+cmeehan1991@users.noreply.github.com>
2024-05-13 06:46:46 -0400

    Updating route management

--- src/app/lib/constants/routes.ts
+++ src/app/lib/constants/routes.ts
@@ -1,5 +1,7 @@
-export const BASE = `http://localhost:8080`;
+import { isDevMode } from "@angular/core";
+//export const BASE = `http://localhost:8080`;
 //export const BASE = `https://api.utterfare.com`;
+export const BASE = isDevMode() ? 'http://localhost:8080' : 'https://api.utterfare.com';
 export const VENDOR_PROFILE_ROUTE = `${BASE}/vendor/profile`;
 export const SEARCH_ROUTE = `${BASE}/search`;
 export const MENU_ITEM_ROUTE = `${BASE}/vendor/items`;
